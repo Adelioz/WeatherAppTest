01c3385202db3e07a8504be66618596c5d7f1d15
Адель Рахимов <rahimov.adel@yandex.ru>
2020-03-03 18:39:56 +0300

    Initial Commit

---
+++ WeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestApp
+//
+//  Created by Адель Рахимов on 03.03.2020.
+//  Copyright © 2020 Адель Рахимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
